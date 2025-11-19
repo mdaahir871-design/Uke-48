3915cad1e961ac054eda493b9722a14fbcaa83eb
mdaahir871-design <mdaahir871@gmail.com>
2025-11-19 12:19:10 +0100

    Her lagde Henrik og jeg en kode der det Tar en nummer og skjekker om det er en Partall eller en oddetall. ved å lage if og else if statements. også lagde vi en knap som står "skjekk".

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Partall from './Components/Partall.jsx'
 import Tommer from './Components/Tommer.jsx'
 
 function App() {
   return (
     <>
       <Tommer />
+      <Partall />
     </>
   )
 }
